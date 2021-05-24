cf2e503c964598f7022ed5b7b25b4a24ea546658
Keith Funk <Keith.C.Funk@gmail.com>
2021-05-23 20:47:16 -0600

    commit tree 2df90f977f28768c3e11a81881f25ac4ec858c0f

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+
+    constructor(props){//props that constructor gets from its parent
+        super()
+        this.state= {
+            secondsLeft: props.initialCount
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft + ' seconds left before I go boom!'}
+            </div>
+        )
+    }
+}
